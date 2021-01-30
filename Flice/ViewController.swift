096067af06b60fbc83766410a2c6bbc82da33377
Makwan BK <contact@makwanbk.com>
2021-01-30 13:24:31 +0300

    Initial Commit

---
+++ Flice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flice
+//
+//  Created by Makwan BK on 2021-01-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
