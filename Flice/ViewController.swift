ec5e90bb5d5a80efacaf89a5df0575fb071d5058
Makwan BK <contact@makwanbk.com>
2021-01-31 00:26:46 +0300

    Initial Commit

--- Flice/ViewController.swift
+++ Flice/ViewController.swift
@@ -10,6 +10,17 @@ import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var statusEmoji: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    private var blurView : UIView! {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurredView = UIVisualEffectView(effect: blurEffect)
+        
+        blurredView.frame = statusView.bounds
+        
+        return blurredView
+    }
     
     var session: AVCaptureSession!
     
@@ -20,6 +31,11 @@ class ViewController: UIViewController {
     
     var videoOutput : AVCaptureVideoDataOutput!
     
+    enum EmojiStatus {
+        case happy
+        case eyeClosed
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,11 +43,26 @@ class ViewController: UIViewController {
         startSession()
         
     }
-
+    
     func mainConfiguration() {
-        //TODO:
+        
+        title = "Flice"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.triangle.2.circlepath.camera"), style: .plain, target: self, action: #selector(flipCamera))
+        
+        
+        statusView.clipsToBounds = true
+        statusView.layer.cornerRadius = 15
+        statusView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        statusEmoji.text = "🧐"
+        
+        statusLabel.text = "Searching for your status"
+        statusLabel.font = .init(descriptor: UIFont.systemFont(ofSize: 22, weight: .medium).fontDescriptor.withDesign(.rounded)!, size: 22)
+        
+        statusView.insertSubview(blurView, at: 0)
+        
     }
-
+    
     func startSession() {
         
         DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
@@ -51,7 +82,7 @@ class ViewController: UIViewController {
             DispatchQueue.main.async {
                 self.setPreview()
             }
-           
+            
             setOutput()
             
             session.commitConfiguration()
@@ -73,7 +104,7 @@ class ViewController: UIViewController {
             fatalError()
         }
         
-
+        
         
         backInput = deviceInput
         session.addInput(backInput)
@@ -89,6 +120,7 @@ class ViewController: UIViewController {
         previewLayer.frame = view.layer.frame
         
         view.layer.insertSublayer(previewLayer, above: statusView.layer)
+        previewLayer.addSublayer(blurView.layer)
         previewLayer.addSublayer(statusView.layer)
         
     }
@@ -105,46 +137,76 @@ class ViewController: UIViewController {
             fatalError()
         }
         
+        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String : Int(kCVPixelFormatType_32BGRA)]
+        videoOutput.alwaysDiscardsLateVideoFrames = true
         videoOutput.connection(with: .video)?.isEnabled = true
         
     }
     
+    func imageFromSampleBuffer(_ sampleBuffer : CMSampleBuffer) -> UIImage {
+        
+        let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = (CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let context = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+        let imageRef = context?.makeImage()
+        
+        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let resultImage: UIImage = UIImage(cgImage: imageRef!)
+        
+        return resultImage
+        
+    }
+    
+    func setUIChanges(emoji: String, text: String) {
+        statusEmoji.text = emoji
+        statusLabel.text = text
+    }
+    
+    @objc func flipCamera() {
+        let ac = UIAlertController(title: "Soon", message: "In next update, you'll be able to switch between front and rear cameras.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Close", style: .cancel))
+        
+        present(ac, animated: true, completion: nil)
+    }
     
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
-        guard let cvBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        let image = self.imageFromSampleBuffer(sampleBuffer)
         
-        let ciImage = CIImage(cvImageBuffer: cvBuffer)
-
-
-        let options = [CIDetectorAccuracy : CIDetectorAccuracyLow]
+        let ciImage = CIImage(image: image)!
+        
+        
+        let options = [CIDetectorAccuracy : CIDetectorAccuracyHigh]
         let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)
-
-        let faces = faceDetector?.features(in: ciImage)
-
-        if let face = faces?.first as? CIFaceFeature {
-            print("Found face at \(face.bounds)")
-
-            if face.hasLeftEyePosition {
-                    print("Found left eye at \(face.leftEyePosition)")
-            }
-
-            if face.hasRightEyePosition {
-                print("Found right eye at \(face.rightEyePosition)")
-            }
-
-            if face.hasMouthPosition {
-                print("Found mouth at \(face.mouthPosition)")
-            }
-
+        
+        let faces = faceDetector?.features(in: ciImage, options: [CIDetectorSmile : true, CIDetectorEyeBlink : true])
+        
+        for face in faces as! [CIFaceFeature] {
+            
             if face.hasSmile {
-                print("SMILEEEEE")
+                DispatchQueue.main.async { [unowned self] in
+                    setUIChanges(emoji: "😊", text: "Happy me!")
+                }
+            } else if face.leftEyeClosed && face.rightEyeClosed {
+                DispatchQueue.main.async { [unowned self] in
+                    setUIChanges(emoji: "🙈", text: "Eyes Closed")
+                }
+            } else {
+                DispatchQueue.main.async { [unowned self] in
+                    setUIChanges(emoji: "🧐", text: "Searching for your status")
+                }
             }
         }
-        
     }
     
     
